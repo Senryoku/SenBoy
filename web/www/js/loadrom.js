bcd67f3b552f11e35c8af06a7340888e9cd97a2a
Senryoku <maretverdant@gmail.com>
2018-06-18 15:37:16 +0200

    Drag&Drop on web interface

--- web/www/js/loadrom.js
+++ web/www/js/loadrom.js
@@ -25,16 +25,7 @@ function read_local_file(e) {
 	if (!file) {
 		return;
 	}
-	var reader = new FileReader();
-	reader.onload = function(e) {
-		var contents = e.target.result;
-		var buf = Module._malloc(contents.byteLength);
-		var dataHeap = new Uint8Array(Module.HEAPU8.buffer, buf, contents.byteLength);
-		dataHeap.set(new Uint8Array(contents));
-		Module.ccall('load_rom', 'number', ['number', 'number'], [dataHeap.byteOffset, contents.byteLength]);
-		Module._free(buf);
-	};
-	reader.readAsArrayBuffer(file);
+	load_file_to_memory(file);
 }
 
 document.getElementById('file-input')
@@ -55,6 +46,60 @@ function local_server_rom(r)
 	read_server_side_file(path);
 }
 
-document.getElementById('server-roms')
-  .addEventListener('click', local_server_rom, false);
-  
+document.getElementById('server-roms').addEventListener('click', local_server_rom, false);
+
+document.querySelector("body").addEventListener("dragover", function (ev) { ev.preventDefault(); });
+document.querySelector("body").addEventListener("drop", function (ev) {
+	console.log('File(s) dropped');
+
+	// Prevent default behavior (Prevent file from being opened)
+	ev.preventDefault();
+    ev.stopPropagation();
+
+	if (ev.dataTransfer.items) {
+		// Use DataTransferItemList interface to access the file(s)
+		for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+			// If dropped items aren't files, reject them
+			if (ev.dataTransfer.items[i].kind === 'file') {
+				var file = ev.dataTransfer.items[i].getAsFile();
+				console.log(file.name);
+				if(load_file_to_memory(file))
+					break;
+			}
+		}
+	} else {
+		// Use DataTransfer interface to access the file(s)
+		for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+			console.log(ev.dataTransfer.files[i].name);
+			if(load_file_to_memory(file))
+				break;
+		}
+	} 
+
+	// Pass event to removeDragData for cleanup
+	removeDragData(ev)
+});
+
+function removeDragData(ev) {
+	if (ev.dataTransfer.items) {
+		// Use DataTransferItemList interface to remove the drag data
+		ev.dataTransfer.items.clear();
+	} else {
+		// Use DataTransfer interface to remove the drag data
+		ev.dataTransfer.clearData();
+	}
+}
+
+function load_file_to_memory(file) {
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		var contents = e.target.result;
+		var buf = Module._malloc(contents.byteLength);
+		var dataHeap = new Uint8Array(Module.HEAPU8.buffer, buf, contents.byteLength);
+		dataHeap.set(new Uint8Array(contents));
+		Module.ccall('load_rom', 'number', ['number', 'number'], [dataHeap.byteOffset, contents.byteLength]);
+		Module._free(buf);
+	};
+	reader.readAsArrayBuffer(file);
+	return true;
+}
